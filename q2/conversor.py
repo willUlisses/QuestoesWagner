8490bbaa3bfa874278e9a33c30b24f099a61f07c
willUlisses <williamcavalcante06@gmail.com>
2025-10-01 16:34:50 -0300

    Resposta q2

---
+++ q2/conversor.py
@@ -0,0 +1,5 @@
+def conversor_para_celsius(fahrenheit):
+    return ((fahrenheit - 32) * 5) / 9
+
+def conversor_para_fahrenheit(celsius):
+    return (1.8 * celsius) + 32
--- q2/main.py
+++ q2/main.py
@@ -2,6 +2,8 @@
 # Conversão entre Celsius e Fahrenheit
 # Obrigração: criar um arquivo conversor.py e importar as funções
 
+from conversor import conversor_para_celsius, conversor_para_fahrenheit
+
 print("Conversor de Temperatura")
 print("1. Celsius para Fahrenheit")
 print("2. Fahrenheit para Celsius")
@@ -9,7 +11,9 @@ opcao = int(input("Escolha uma opção (1 ou 2): "))
 
 if opcao == 1:
     celsius = float(input("Digite a temperatura em Celsius: "))
+    print(conversor_para_fahrenheit(celsius))
 elif opcao == 2:
     fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
+    print(conversor_para_celsius(fahrenheit))
 else:
     print("Opção inválida.")
