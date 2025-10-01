55a5dbd4bb94e64413600581ae2aa3ca3e266502
willUlisses <williamcavalcante06@gmail.com>
2025-10-01 16:47:24 -0300

    Resposta q4

--- q4/main.py
+++ q4/main.py
@@ -1,8 +1,7 @@
 # Questão 4 - Verificador de Números Pares e Ímpares
 # Crie uma função que receba um número inteiro e retorne se ele é par ou ímpar.
+from verificador import verifica_numero
 
 numero = int(input("Digite um número: "))
-if numero % 2 == 0:
-    print("Par")
-else:
-    print("Ímpar")
+print(verifica_numero(numero))
+
---
+++ q4/verificador.py
@@ -0,0 +1,2 @@
+def verifica_numero(numero):
+    return "Par" if numero % 2 == 0 else "Ímpar"
