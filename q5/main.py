de616d123839e48c0e552779570359f86a4bc87a
willUlisses <williamcavalcante06@gmail.com>
2025-10-01 16:56:35 -0300

    Resposta q5

--- q5/main.py
+++ q5/main.py
@@ -31,4 +31,5 @@ def main():
         print("Nenhuma nota foi informada.")
 
 
+
 main()
---
+++ q5/notas.py
@@ -0,0 +1,8 @@
+def media(notas_aluno):
+    return sum(notas_aluno) / len(notas_aluno)
+
+def maior(notas_aluno):
+    return max(notas_aluno)
+
+def menor(notas_aluno):
+    return min(notas_aluno)
