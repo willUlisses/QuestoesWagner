ac8fbf971922acf792e69fd2721db8bf8b1177e9
willUlisses <williamcavalcante06@gmail.com>
2025-10-04 13:50:32 -0300

    Fix: Resposta q6

--- q6/conversor.py
+++ q6/conversor.py
@@ -1,13 +1,16 @@
 def real_para_dolar():
     valor = float(input("Digite o valor em reais: "))
     cotacao = float(input("Informe a cotação: "))
-    print(f"R${valor} em reais equivalem a U${(valor / cotacao):.2f} em dólares")
+    print(f"R${valor:.2f} em reais equivalem a U${(valor / cotacao):.2f} em dólares")
 
 def dolar_para_real():
     valor = float(input("Digite o valor em dólar: "))
     cotacao = float(input("Informe a cotação: "))
-    print(f"U${valor} dólares equivalem a R${(valor * cotacao):.2f} em reais")
+    print(f"U${valor:.2f} dólares equivalem a R${(valor * cotacao):.2f} em reais")
 
 
-
-
+def conversorDeMoedas(tipo=1):
+    if tipo == 1:
+        real_para_dolar()
+    else:
+        dolar_para_real()
--- q6/main.py
+++ q6/main.py
@@ -2,19 +2,19 @@
 # Crie a função que converte reais para dólares e dólares para reais (arquivo conversor.py)
 # real_para_dolar e dolar_para_real. Caso o usuário não passe o tipo (real_para_dolar ou dolar_para_real) o default deve ser real_para_dolar <- padrão
 
-from conversor import *
+from conversor import conversorDeMoedas
 
 print("Conversor entre dólares e reais\n")
 
-tipo = int(input("Agora informe o tipo de conversão\n[1] - Real -> Dolar \n[2] - Dolar -> Real\n"))
+tipo = int(input("Agora informe o tipo de conversão\n[1] - Real -> Dolar \n[2] - Dolar -> Real\n[3] - não inserir\nescolha: "))
 
 match tipo:
     case 1:
-        real_para_dolar() 
+        conversorDeMoedas(1) 
     case 2:
-        dolar_para_real() 
+        conversorDeMoedas(2) 
     case _:
-        real_para_dolar()        
+        conversorDeMoedas()        
 
 
 
